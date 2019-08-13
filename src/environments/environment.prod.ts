8bbe4f6c89217e49a749ba5d43253bebd4c5729e
Ravi Kumar <kammari.au@gmail.com>
2019-08-13 22:02:28 +1000

    Update API KEy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   urlPath: "https://api.themoviedb.org/3/authentication/token/new/",
   apiPath: "https://api.themoviedb.org/3/",
   imagePath: "https://image.tmdb.org/t/p/",
-  apiKey: "bf9c6c0e33973090ea32512ec98d62ac"
+  apiKey: "*****"
 };
